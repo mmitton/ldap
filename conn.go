f0496261d5de87e0b576354b212df42be87756db
Baris Metin <bmetin@yelp.com>
2013-07-26 16:49:08 -0700

    merge conn fix from https://github.com/hsoj/ldap/pull/2

--- conn.go
+++ conn.go
@@ -24,7 +24,7 @@ type Conn struct {
 	chanProcessMessage chan *messagePacket
 	chanMessageID      chan uint64
 
-	closeLock sync.Mutex
+	closeLock sync.RWMutex
 }
 
 // Dial connects to the given address on the given network using net.Dial
@@ -239,8 +239,10 @@ func (l *Conn) processMessages() {
 					fmt.Printf("Unexpected Message Result: %d\n", message_id)
 					ber.PrintPacket(message_packet.Packet)
 				} else {
-					go func() { chanResult <- message_packet.Packet }()
-					// chanResult <- message_packet.Packet
+					packetCopy := message_packet.Packet
+					go func() {
+						chanResult <- packetCopy
+					}()
 				}
 			case MessageFinish:
 				// Remove from message list
@@ -254,6 +256,9 @@ func (l *Conn) processMessages() {
 }
 
 func (l *Conn) closeAllChannels() {
+	l.closeLock.Lock()
+	defer l.closeLock.Unlock()
+
 	fmt.Printf("closeAllChannels\n")
 	for MessageID, Channel := range l.chanResults {
 		if l.Debug {
@@ -289,8 +294,9 @@ func (l *Conn) reader() {
 
 		message_id := p.Children[0].Value.(uint64)
 		message_packet := &messagePacket{Op: MessageResponse, MessageID: message_id, Packet: p}
+
 		if l.chanProcessMessage != nil {
-			l.chanProcessMessage <- message_packet
+			l.sendProcessMessage(message_packet)
 		} else {
 			fmt.Printf("ldap.reader: Cannot return message\n")
 			return
@@ -299,7 +305,12 @@ func (l *Conn) reader() {
 }
 
 func (l *Conn) sendProcessMessage(message *messagePacket) {
-	if l.chanProcessMessage != nil {
-		go func() { l.chanProcessMessage <- message }()
-	}
+	go func() {
+		l.closeLock.RLock()
+		defer l.closeLock.RUnlock()
+
+		if l.chanProcessMessage != nil {
+			l.chanProcessMessage <- message
+		}
+	}()
 }
